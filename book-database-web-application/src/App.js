976725aa25d7342069e9b5b8854ba03ac2337392
Junho <js5993@nyu.edu>
2020-03-18 13:11:32 -0700

    some frontend components written, more to come

--- book-database-web-application/src/App.js
+++ book-database-web-application/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router,Route} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Navbar from "./components/navbar.component";
+import BooksList from "./components/books-list.component";
+import EditBook from "./components/edit-book.component";
+import CreateBook from "./components/create-book.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={BooksList} />
+      <Route path="/edit/:id" component={EditBook} />
+      <Route path="/create" component={CreateBook} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
