35e636721575bdcb4c57bb2d7e863ffb05d166c6
Junho <js5993@nyu.edu>
2020-03-18 15:44:12 -0700

    create book entry frontend created, not rigged to server yet

--- book-database-web-application/src/App.js
+++ book-database-web-application/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router,Route} from "react-router-dom";
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 
@@ -13,12 +13,14 @@ import CreateUser from "./components/create-user.component";
 function App() {
   return (
     <Router>
+      <div className="container">
       <Navbar />
       <br/>
       <Route path="/" exact component={BooksList} />
       <Route path="/edit/:id" component={EditBook} />
       <Route path="/create" component={CreateBook} />
       <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
