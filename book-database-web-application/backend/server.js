f50941787253748270e074496f19e61f0b6b6c49
Junho <js5993@nyu.edu>
2020-03-18 22:46:14 -0700

    xhr failed loading error is back but POST actions are happening correctly on the server ... not sure what is going on, could be a server problem, unable to get Google Books API responses through anymore

--- book-database-web-application/backend/server.js
+++ book-database-web-application/backend/server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.options('*', cors());
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
@@ -19,11 +20,11 @@ connection.once('open', () => {
     console.log('MongoDB data connection established successfully');
 });
 
-const books = require('./routes/books');
-const users = require('./routes/users');
+const booksRouter = require('./routes/books');
+const usersRouter = require('./routes/users');
 
-app.use('/books', books);
-app.use('/users', users);
+app.use('/books', booksRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log('Server is running on port:  ${port}');
